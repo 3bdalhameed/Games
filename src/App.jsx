9c4be4140591165bfa2401de451088d497823c4b
3bdalhameed <92941574+3bdalhameed@users.noreply.github.com>
2025-04-07 23:46:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MyTest from "./pages/Main";
-import Qr from "./pages/QR";
 import GotHacked from "./pages/GotHacked";
 
 import "./App.css";
@@ -13,9 +11,7 @@ function App() {
         <Router>
             <div>
                 <Routes>
-                    <Route path="/" element={<MyTest />} />
-                    <Route path="/qr" element={<Qr />} />
-                    <Route path="/gothacked" element={<GotHacked />} />
+                    <Route path="/" element={<GotHacked />} />
                 </Routes>
             </div>
         </Router>
